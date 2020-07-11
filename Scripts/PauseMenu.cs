f3f96fec2e06b21cf6e490e55852f31be40fa752
MK <simon365@naver.com>
2020-07-11 23:53:28 +0900

    ver 1.13 pause menu added, player now can stop game while Playing

---
+++ Scripts/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject go_Base;
+
+
+    public void CallMenu()
+    {
+        
+        go_Base.gameObject.SetActive(true);
+        Time.timeScale = 0f;        //일시정지창 실행시 타임스케일 0, 시간흐름 정지
+    }
+
+    public void CloseMenu()
+    {
+        
+        go_Base.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+}
